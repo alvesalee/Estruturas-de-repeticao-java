2cf20fe0787e5e4dc82475d8233d44fab8176565
ALEXANDRE ALVES SILVA <105252517+alvesalee@users.noreply.github.com>
2022-10-24 21:07:23 -0300

    Add files via upload

---
+++ ExemploFor.java
@@ -0,0 +1,13 @@
+package EstruturasDeRepeticao;
+
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        String alunos[] = {" FELIPE", " JONAS", " GUILHERME"," ISABELA"};
+
+        for (int x=0; x< alunos.length; x++ ) {
+            System.out.println("O aluno no indice x= " + x + alunos[x]);
+        }
+        
+    }
+}
